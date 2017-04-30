3723d7f92d1f316351cc3eddb45fa620c8f7372f
SandraR <srodrig284@gmail.com>
2017-04-30 17:32:09 -0400

    Working App

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -32,7 +32,7 @@ router.put("/:id", function(req, res) {
     console.log("condition", condition);
 
     burger.update({
-        devoured: true
+        devoured: req.body.devoured
     }, condition, function() {
         res.redirect("/");
     });
